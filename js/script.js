1e512b900affac03b4e4ef45998a9db077d2f7f7
Keviiin-999 <yahirkevin206@gmail.com>
2025-04-04 22:14:14 -0600

    Primer commit del proyecto

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.btn-success');
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            alert('Producto agregado al carrito');
+        });
+    });
+});
