3a73dd635f541eccbb1f55e848c4e2763d52a747
Keviiin-999 <yahirkevin206@gmail.com>
2025-04-10 13:17:19 -0600

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('.btn-success');
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            alert('Producto agregado al carrito');
-        });
-    });
-});
